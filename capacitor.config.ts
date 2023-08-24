0552d2a060381e14e6565750ae78c3e47922e179
srhafid <hafidmosquera@gmail.com>
2023-08-24 10:11:54 -0500

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'SchoolProyect',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
