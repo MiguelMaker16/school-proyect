ba70a20c855acba13c1e30e0816b44be79f9c627
BoondDark Dark <miguelmaker16@gmail.com>
2023-08-30 17:09:08 -0500

    routers security with fire auth-guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { canActivate, redirectUnauthorizedTo } from '@angular/fire/auth-guard'
 
 const routes: Routes = [
   {
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'classroom',
+    loadChildren: () => import('./classroom/classroom.module').then( m => m.ClassroomPageModule),
+    ...canActivate(() => redirectUnauthorizedTo(['/login'])) 
   }
 
 ];
